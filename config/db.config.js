fc8604fc5da34452d3f5c49077f19ad1f90d264a
matus_vrsansky <matuss182@gmail.com>
2022-10-26 21:00:26 +0200

    initial commit 4

--- config/db.config.js
+++ config/db.config.js
@@ -3,11 +3,5 @@ module.exports = {
     USER: "freedb_matus",
     PASSWORD: "xZVCxX6EwN!h28v",
     DB: "freedb_weatherstation",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    dialect: "mysql"
   };
