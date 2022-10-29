eb0dff711e4984916faf4565c6479de5ddfac1dc
matus_vrsansky <matuss182@gmail.com>
2022-10-29 17:30:12 +0200

    revert

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 //const cors = require("cors");
 const app = express();
-const cors = require("cors");
+//const cors = require("cors");
 
  
 //const http = require('http').createServer(app);
