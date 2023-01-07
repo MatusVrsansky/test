838adbae1690e3f549fbef374533b9e79b47e581
matus_vrsansky <matuss182@gmail.com>
2023-01-08 00:59:29 +0100

    cors test deploy

--- handlers/checkNotifications.js
+++ handlers/checkNotifications.js
@@ -393,6 +393,8 @@ module.exports = () => {
             })
             .then(respond => {
             // console.log(user.email)
+            console.log("respondd")
+            console.log(respond);
             sendSms = respond.value;
             console.log("Respond for SMS: " + sendSms);
             });
